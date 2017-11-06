7e0c66126be23a15c3a2ae9da8a2a1eac1953e0b
sananth12 <sananthanatarajan12@gmail.com>
2017-11-05 21:39:13 -0500

    HotFix chrome conceal issue - fix this later

--- js/main.js
+++ js/main.js
@@ -12,19 +12,19 @@
       }, 1500);
     });
     mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    if ($("html").hasClass("cssanimations") && !mobile) {
-      animate = function(animation) {
-        return $(animation[0]).addClass("concealed").viewportChecker({
-          classToAdd: "visible animated " + (animation[1].trim()),
-          offset: 200
-        });
-      };
-      items = [['#about p', 'fadeInUp'], ['.column', 'fadeInUp'], ['.item', 'fadeInDown'], ['.bar', 'fadeInRight'], ['.paperplane', 'bounceInDown'], ['#quote .container', 'fadeIn'], ['#quote .icon', 'fadeInDown']];
-      for (_i = 0, _len = items.length; _i < _len; _i++) {
-        item = items[_i];
-        animate(item);
-      }
-    }
+    // if ($("html").hasClass("cssanimations") && !mobile) {
+    //   animate = function(animation) {
+    //     return $(animation[0]).addClass("concealed").viewportChecker({
+    //       classToAdd: "visible animated " + (animation[1].trim()),
+    //       offset: 200
+    //     });
+    //   };
+    //   items = [['#about p', 'fadeInUp'], ['.column', 'fadeInUp'], ['.item', 'fadeInDown'], ['.bar', 'fadeInRight'], ['.paperplane', 'bounceInDown'], ['#quote .container', 'fadeIn'], ['#quote .icon', 'fadeInDown']];
+    //   for (_i = 0, _len = items.length; _i < _len; _i++) {
+    //     item = items[_i];
+    //     animate(item);
+    //   }
+    // }
     return $("#footer ul li").hover(function() {
       return $(this).find('p').stop(true, true).fadeIn(800);
     }, function() {
